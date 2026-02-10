e2d0c7504c0648e40eeafef9cd035da5f8fc18f2
Georgi Angelov <gtangelov76@gmail.com>
2026-02-10 22:22:51 +0200

    Update memory_game.py

---
+++ Programming Fundamentals Mid Exam 1/memory_game.py
@@ -0,0 +1,39 @@
+sequence_of_elements = list(map(int, input().split()))
+cloned = [x for x in sequence_of_elements for _ in range(2)]
+moves = 0
+right_guesses = 0
+is_successful = False
+while True:
+    command = input()
+    if command == "end":
+        break
+    moves += 1
+    parts = command.split()
+    first_idx = int(parts[0])
+    second_idx = int(parts[1])
+    if (
+        first_idx == second_idx or
+        not 0 <= first_idx < len(cloned) or
+        not 0 <= second_idx < len(cloned)
+    ):
+        print("Invalid input! Adding additional elements to the board")
+        index = len(cloned) // 2
+        penalty = f"{moves}-a"
+        cloned.insert(index, penalty)
+        cloned.insert(index, penalty)
+
+    if cloned[first_idx] == cloned[second_idx]:
+        print(f"Congrats! You have found matching elements - {cloned[first_idx]}")
+        cloned = [num for num in cloned if num !=cloned[first_idx]]
+        right_guesses += 1
+        if right_guesses == len(cloned) // 2:
+            is_successful = True
+            break
+    elif cloned[first_idx] != cloned[second_idx]:
+        print("Try again!")
+
+if is_successful:
+    print(f"You have won in {moves} turns!")
+else:
+    print(f"Sorry you lose :(\n"
+    f"{cloned}")
