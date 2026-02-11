54e555db7dd72c85ae314c526cddd14bd7880a81
Georgi Angelov <gtangelov76@gmail.com>
2026-02-11 22:12:32 +0200

    Update numbers.py

--- Programming Fundamentals Mid Exam 2/numbers.py
+++ Programming Fundamentals Mid Exam 2/numbers.py
@@ -2,6 +2,7 @@ sequence_of_integers = list(map(int, input().split()))
 
 total_sum = sum(sequence_of_integers)
 avg_value = total_sum / len(sequence_of_integers)
+
 numbers_above_average = sorted([num for num in sequence_of_integers if num > avg_value], reverse=True)
 
 if not numbers_above_average:
