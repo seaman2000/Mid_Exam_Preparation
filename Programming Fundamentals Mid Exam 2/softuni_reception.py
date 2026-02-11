b879038d0d0f69a747880a7b19021ad16c5a579b
Georgi Angelov <gtangelov76@gmail.com>
2026-02-11 21:25:42 +0200

    Create softuni_reception.py

---
+++ Programming Fundamentals Mid Exam 2/softuni_reception.py
@@ -0,0 +1,16 @@
+first_employee = int(input())
+second_employee = int(input())
+third_employee = int(input())
+
+students_count = int(input())
+
+students_per_hour_handled = first_employee + second_employee + third_employee
+hours = 0
+while students_count > 0:
+    hours += 1
+
+    if hours % 4 != 0:
+        students_count -= students_per_hour_handled
+
+print(f"Time needed: {hours}h.")
+
