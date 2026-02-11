ee3c86a6b8511a03769b363aae56c07347c4313c
Georgi Angelov <gtangelov76@gmail.com>
2026-02-11 21:36:53 +0200

    Create array_modifier.py

---
+++ Programming Fundamentals Mid Exam 2/array_modifier.py
@@ -0,0 +1,25 @@
+integers_array = list(map(int, input().split()))
+
+while True:
+    command = input()
+    
+    if command == "end":
+        break
+    parts = command.split()
+    type_of_action = parts[0]
+
+    if type_of_action == "swap":
+        first_element = parts[1]
+        second_element = parts[2]
+
+    elif type_of_action == "multiply":
+        first_element = int(parts[1])
+        second_element = int(parts[2])
+        result = integers_array[first_element] * integers_array[second_element]
+        integers_array.pop(integers_array[first_element])
+        integers_array.insert(first_element - 1, result)
+
+    elif type_of_action == "decrease":
+        integers_array = [num - 1 for num in integers_array]
+
+print(', '.join(map(str, integers_array)))
