e40882f67d08707284a6ee3c7e53d680fece14dd
Georgi Angelov <gtangelov76@gmail.com>
2026-02-11 22:08:51 +0200

    Create numbers.py

---
+++ Programming Fundamentals Mid Exam 2/numbers.py
@@ -0,0 +1,11 @@
+sequence_of_integers = list(map(int, input().split()))
+
+total_sum = sum(sequence_of_integers)
+avg_value = total_sum / len(sequence_of_integers)
+numbers_above_average = sorted([num for num in sequence_of_integers if num > avg_value], reverse=True)
+
+if not numbers_above_average:
+    print("No")
+else:
+    lst = numbers_above_average[:5]
+    print(' '.join(map(str, lst)))
