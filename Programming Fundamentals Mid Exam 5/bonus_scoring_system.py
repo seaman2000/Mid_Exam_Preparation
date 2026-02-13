1d3f258c4e7f37bc1dc232eba1751ecd11d1c418
Georgi Angelov <gtangelov76@gmail.com>
2026-02-13 22:31:52 +0200

    Update bonus_scoring_system.py

---
+++ Programming Fundamentals Mid Exam 5/bonus_scoring_system.py
@@ -0,0 +1,16 @@
+number_of_students = int(input())
+number_of_lectures = int(input())
+additional_bonus = int(input())
+max_bonus = 0
+student_attendances = 0
+
+for student in range(number_of_students):
+    attendances = int(input())
+    if number_of_lectures > 0:
+        total_bonus = attendances / number_of_lectures * (5 + additional_bonus)
+        if total_bonus > max_bonus:
+            max_bonus = total_bonus
+            student_attendances = attendances
+
+print(f"Max bonus: {round(max_bonus)}.")
+print(f"The student has attended {round(student_attendances)} lectures.")
