cbca70609b73cf8a60d93ae9fd72854df63c57ff
Georgi Angelov <gtangelov76@gmail.com>
2026-02-13 12:17:25 +0200

    Update shoot_for_win.py

--- Programming Fundamentals Mid Exam 3/shoot_for_win.py
+++ Programming Fundamentals Mid Exam 3/shoot_for_win.py
@@ -4,26 +4,21 @@ while True:
     command = input()
 
     if command == "End":
-        print(f"Shot targets: {shooting_count} -> {' '.join(sequence_of_integers)}")
+        print(f"Shot targets: {shooting_count} -> {' '.join(map(str, sequence_of_integers))}")
         break
     idx = int(command)
+    if idx in range(len(sequence_of_integers)):
 
-    if sequence_of_integers[idx] == -1:
+        if sequence_of_integers[idx] != -1:
 
-        if idx in len(sequence_of_integers):
+            value = sequence_of_integers[idx]
+            for num in range(len(sequence_of_integers)):
+                if sequence_of_integers[num] != -1:
+                    if sequence_of_integers[num] <= value:
+                        sequence_of_integers[num] += value
+                    else:
+                        sequence_of_integers[num] -= value
 
-            sequence_of_integers = [
-                num +sequence_of_integers[idx]
-                for num in sequence_of_integers
-                if num < sequence_of_integers[idx]
-                ]
-
-            sequence_of_integers = [
-                num - sequence_of_integers
-                for num in sequence_of_integers
-                if num > sequence_of_integers[idx]
-            ]
-            
             sequence_of_integers[idx] = -1
             shooting_count += 1
 
