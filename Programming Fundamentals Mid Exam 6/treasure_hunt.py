49f69cd2847f75000d314e3053642333fc4667d2
Georgi Angelov <gtangelov76@gmail.com>
2026-02-14 12:53:46 +0200

    Update treasure_hunt.py

---
+++ Programming Fundamentals Mid Exam 6/treasure_hunt.py
@@ -0,0 +1,19 @@
+initial_loot = input().split("|")
+while True:
+    command = input()
+    if command == "Yohoho!":
+        break
+    parts = command.split()
+    action = parts[0]
+    if action == "Loot":
+        for idx, item in enumerate(parts):
+            if idx > 0 and item not in initial_loot:
+                initial_loot.insert(0, item)
+    elif action == "Drop":
+        index = int(parts[1])
+        if 0 <= index < len(initial_loot):
+            initial_loot.remove(initial_loot[index])
+            initial_loot.append(initial_loot[index])
+    elif action == "Steal":
+        count = int(parts[1])
+        del initial_loot[:count - 1:-1]
