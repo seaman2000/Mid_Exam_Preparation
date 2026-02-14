c51c60bca55d2c1fadee0167df743fb01a0ecc66
Georgi Angelov <gtangelov76@gmail.com>
2026-02-14 14:10:47 +0200

    Update manowar.py

--- Programming Fundamentals Mid Exam 6/manowar.py
+++ Programming Fundamentals Mid Exam 6/manowar.py
@@ -1,11 +1,15 @@
-def pirate_idx_is_valid(index: int, type_of_action: str):
-    if type_of_action == "Fire":
-        if 0 <= index < len(pirate_ship_status):
-            return True
-    if type_of_action == "Defend":
-        if 0 <= index < len(warship_status):
-            return True
-    return False
+def fire_index_is_valid(index: int):
+    return 0 <= index < len(pirate_ship_status)
+
+
+def defend_indices_are_valid(start:int , end: int):
+    return (
+        0 <= start < len(pirate_ship_status)
+        and
+        0 <= end < len(pirate_ship_status)
+        and
+        start <= end
+    )
 
 
 pirate_ship_status = list(map(int, input().split(">")))
@@ -19,6 +23,26 @@ while True:
     parts_of_command = command.split()
     action = parts_of_command[0]
     index = int(parts_of_command[1])
+
     if action == "Fire":
-        if idx_is_valid(index, action):
+        if fire_index_is_valid(index):
+            damage = int(parts_of_command[2])
+            warship_status[index] -= damage
+            if warship_status[index] <= 0:
+                print(f"You won! The enemy ship has sunken.")
+                break
+
+    elif action == "Defend":
+        start_index = index
+        end_index = int(parts_of_command[2])
+        if defend_indices_are_valid(start_index, end_index):
+            damage = int(parts_of_command[3])
+            for idx in range(start_index, end_index + 1):
+                pirate_ship_status[idx] -= damage
+                if pirate_ship_status[idx] <= 0:
+                    print(f"You lost! The pirate ship has sunken.")
+                    break
+
+    elif action == "Repair":
+        
 
